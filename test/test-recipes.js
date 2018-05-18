d686d1c5ba9501d5ea451c0088d2cc1f2aa8dc92
John Joo <john@Johns-iMac.local>
2018-05-17 19:10:38 -0700

    added test-recipes

--- test/test-recipes.js
+++ test/test-recipes.js
@@ -1,27 +1,78 @@
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 
-const {app, runServer, closeServer} = require('../recipes');
+const {app, runServer, closeServer} = require('../server');
 
-// this lets us use *expect* style syntax in our tests
-// so we can do things like `expect(1 + 1).to.equal(2);`
-// http://chaijs.com/api/bdd/
 const expect = chai.expect;
 
-// This let's us make HTTP requests
-// in our tests.
-// see: https://github.com/chaijs/chai-http
 chai.use(chaiHttp);
 
+describe('Recipes', function() {
+  it('should list recipes on GET', function() { 
+    return chai.request(app)
+      .get('/recipes')
+      .then(function(res) { 
+        expect(res).to.have.status(200); 
+        expect(res).to.be.json; 
+        expect(res.body).to.be.a('array'); 
+        expect(res.body.length).to.be.at.least(1); 
+        const expectedKeys = ['id', 'name', 'checked'];
+        res.body.forEach(function(item) {
+          expect(item).to.be.a('object');
+          expect(item).to.include.keys('id', 'name', 'ingredients');
+        });
+      });
+    });
+    
+  it('should add a recipe on POST', function() {
+    const newItem = {name: 'salad', ingredients: ['lettuce', 'dressing']};
+    return chai.request(app)
+      .post('/recipes')
+      .send(newItem)
+      .then(function(res) {
+        expect(res).to.have.status(201);
+        expect(res).to.be.json; 
+        expect(res.body).to.be.a('object'); 
+        expect(res.body).to.include.keys('id', 'name', 'ingredients'); 
+        expect(res.body.name).should.equal(newRecipe.name);
+        expect(res.body.ingredients).to.be.a('array'); 
+        res.body.ingredients.should.include.members(newRecipe.ingredients); 
+      });
+    }); 
 
-describe('Shopping List', function() {
+    it('should update recipes on PUT', function () {
+      const updateData = { 
+        name: 'salad', 
+        ingredients: ['lettuce', 'dressing']
+      };
+      return chai.request(app)
+        .get('/recipes')
+        .then(function(res) {
+          updateData.id = res.body[0].id; 
+          return chai.request(app)
+            .put('/recipes/${updateData.id}')
+            .send(updateData)
+        })
+        .then(function(res) { 
+          expect(res).to.have.status(200); 
+        });
+    });
 
-  // Before our tests run, we activate the server. Our `runServer`
-  // function returns a promise, and we return the that promise by
-  // doing `return runServer`. If we didn't return a promise here,
-  // there's a possibility of a race condition where our tests start
-  // running before our server has started.
-  before(function() {
+    it('should delete items on DELETE', function() { 
+      return chai.request(app)
+        .get('/recipes')
+        .then(function(res) { 
+          return chai.request(app)
+            .delete('/recipes/${res.body[0].id}');
+        })
+        .then(function(res) { 
+          expect(res).to.have.status(204); 
+        });
+    }); 
+    
+   
+
+  before (function() {
     return runServer();
   });
 
